4e101ffe2b45f2fa54ed5798be8db19084972cef
Volnei Tonato <volneitonato@gmail.com>
2020-08-14 12:07:36 -0300

    add tooltip component proffy-web

---
+++ proffy-web/src/components/Tooltip/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+interface TooltipoProps {
+    title: string;
+    className?: string;
+}
+
+const Tooltip: React.FC<TooltipoProps> = ({
+    title,
+    className = '',
+    children,
+}) => {
+    return (
+        <Container className={className}>
+            {children}
+            <span>{title}</span>
+        </Container>
+    );
+};
+
+export default Tooltip;
